e48cdede146b4eafd19cfdb56bb3566afc5bb05d
Hao Lingjie <michael-hao@foxmail.com>
2020-10-17 19:05:45 +0800

    Add build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+export PRJ_ROOT=`pwd`
+OPENWRT_ROOT=$PRJ_ROOT/openwrt-dev
+CUSTOM_ROOT=$PRJ_ROOT/custom
+
+R2S_DEFCONFIG_FILE=r2s_defconfig
+
+cd $CUSTOM_ROOT
+cp $R2S_DEFCONFIG_FILE $OPENWRT_ROOT/.config
+
+
+cd $OPENWRT_ROOT
+./scripts/feeds update -a && ./scripts/feeds install -a
+
+make download -j4
+
+make -j $($(nproc)+1) || make -j1 || make -j1 V=s
